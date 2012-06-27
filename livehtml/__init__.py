d2b23c924b8e8bb53e77873384d0f474c20cbdf8
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-06-27 23:37:37 +0300

    Trying to fix ldws.js including issue.

--- livehtml/__init__.py
+++ livehtml/__init__.py
@@ -153,7 +153,7 @@ def start_server(root_path, template_path=None, port=None):
     handler = init_handler(root_path)
     application = web.Application([
         ('/_channel/', ChangeWebSocket, {'handler': handler}),
-        ('/_scripts/jdws.js', ScriptHandler),
+        ('/_scripts/ldws.js', ScriptHandler),
         ('/(.+\.html)', HTMLHandler, {'path': root_path}),
         ('/(.*)', web.StaticFileHandler, {'path': root_path}),
     ], template_path=template_path or root_path)
